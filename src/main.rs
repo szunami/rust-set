af6de5baaeeee5bbefc424678946ee9f8475b059
Sam Szuflita <sam.szuflita@gmail.com>
2020-04-12 21:39:31 -0400

    Fix bug w/ is_set

--- src/main.rs
+++ src/main.rs
@@ -38,18 +38,41 @@ fn main() {
             board.push(pile.pop().unwrap());
             board.push(pile.pop().unwrap());
         }
-        println!("Current board state: {:?}", board);
+        println!("Current board state");
+        print_board(&board);
         println!("Enter a potential set, or type deal.");
 
-        let (card_0, card_1, card_2) = read_and_parse_input(&board);
+        let (card_index_0, card_index_1, card_index_2) = read_input();
+
+        let card_0: &Card = board.get(card_index_0).unwrap();
+        let card_1: &Card = board.get(card_index_1).unwrap();
+        let card_2: &Card = board.get(card_index_2).unwrap();
 
         println!("You chose {:?}, {:?}, {:?}", card_0, card_1, card_2);
 
-        is_set(card_0, card_1, card_2);
+        if is_set(card_0, card_1, card_2) {
+            let mut indices = vec![card_index_0, card_index_1, card_index_2];
+            indices.sort();
+            println!("Nicely done!");
+            board.remove(indices[2]);
+            board.remove(indices[1]);
+            board.remove(indices[0]);
+        } else {
+            println!("Keep looking!")
+        }
     }
 }
 
-fn read_and_parse_input(board: &Vec<Card>) -> (&Card, &Card, &Card) {
+fn print_board(board: &Vec<Card>) {
+    for (index, card) in board.iter().enumerate() {
+        print!("{:?}\t\t", card);
+        if (index + 1) % 3 == 0 {
+            print!("\n");
+        }
+    }
+}
+
+fn read_input() -> (usize, usize, usize) {
     let mut raw_user_input = String::new();
 
     io::stdin()
@@ -65,11 +88,7 @@ fn read_and_parse_input(board: &Vec<Card>) -> (&Card, &Card, &Card) {
     let card_index_1 = parse_card_index(&potential_set_indices, 1) as usize;
     let card_index_2 = parse_card_index(&potential_set_indices, 2) as usize;
 
-    let card_0: &Card = board.get(card_index_0).unwrap();
-    let card_1: &Card = board.get(card_index_1).unwrap();
-    let card_2: &Card = board.get(card_index_2).unwrap();
-
-    return (card_0, card_1, card_2);
+    return (card_index_0, card_index_1, card_index_2);
 }
 
 fn parse_card_index(potential_set_indices: &Vec<&str>, input_index: usize) -> i32 {
@@ -84,19 +103,19 @@ fn is_set(card_0: &Card, card_1: &Card, card_2: &Card) -> bool {
     println!("Checking if {:?}, {:?}, {:?} is a set...", card_0, card_1, card_2);
 
     if !((card_0.color == card_1.color && card_0.color == card_2.color) ||
-        (card_0.color != card_1.color && card_0.color != card_2.color)) {
+        (card_0.color != card_1.color && card_0.color != card_2.color && card_1.color != card_2.color)) {
         println!("Color check failed");
         return false;
     }
 
     if !((card_0.quantity == card_1.quantity && card_0.quantity == card_2.quantity) ||
-        (card_0.quantity != card_1.quantity && card_0.quantity != card_2.quantity)) {
+        (card_0.quantity != card_1.quantity && card_0.quantity != card_2.quantity && card_1.quantity != card_2.quantity)) {
         println!("Quantity check failed");
         return false;
     }
 
     if !((card_0.shading == card_1.shading && card_0.shading == card_2.shading) ||
-        (card_0.shading != card_1.shading && card_0.shading != card_2.shading)) {
+        (card_0.shading != card_1.shading && card_0.shading != card_2.shading && card_1.shading != card_2.shading)) {
         println!("Shading check failed");
         return false;
     }
@@ -200,5 +219,26 @@ mod tests {
         };
 
         assert_eq!(is_set(&card_0, &card_1, &card_2), true)
+    }
+
+    #[test]
+    fn is_set_works_4() {
+        let card_0 = Card {
+            color: PURPLE,
+            quantity: TWO,
+            shading: FULL,
+        };
+        let card_1 = Card {
+            color: RED,
+            quantity: THREE,
+            shading: PARTIAL,
+        };
+        let card_2 = Card {
+            color: RED,
+            quantity: THREE,
+            shading: EMPTY,
+        };
+
+        assert_eq!(is_set(&card_0, &card_1, &card_2), false)
     }
 }
