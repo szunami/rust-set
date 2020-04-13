a1c9176651a85431dfd4853e8798b7dca1033875
Sam Szuflita <sam.szuflita@gmail.com>
2020-04-13 09:41:26 -0400

    Support 3 types of input

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,12 @@ struct Card {
     shape: Shape,
 }
 
+enum InputType {
+    Deal,
+    Help,
+    PotentialSet(usize, usize, usize),
+}
+
 impl Card {
     fn print(&self) -> ColoredString {
         let shape = match self.shape {
@@ -76,27 +82,66 @@ fn main() {
         }
         println!("Current board state");
         print_board(&board);
-        println!("Enter a potential set, or type deal.");
+        println!("Enter help, deal, or input a set.");
 
-        let (card_index_0, card_index_1, card_index_2) = read_input();
+        match read_input() {
+            InputType::Deal => {
+                if !find_set(&board).is_empty() {
+                    println!("There is a set here. Type help if you need help.");
+                } else {
+                    println!("There are no sets. Dealing 3 more cards.");
+                    println!("Dishing out 3 more cards");
+                    board.push(pile.pop().unwrap());
+                    board.push(pile.pop().unwrap());
+                    board.push(pile.pop().unwrap());
+                }
+            }
+            InputType::Help => {
+                let valid_sets = find_set(&board);
+                if valid_sets.is_empty() {
+                    println!("There are no sets here. Type deal.");
+                    return;
+                }
+                println!("{:?}", valid_sets.get(0).unwrap());
+            }
+            InputType::PotentialSet(card_index_0, card_index_1, card_index_2) => {
+                let card_0: &Card = board.get(card_index_0).unwrap();
+                let card_1: &Card = board.get(card_index_1).unwrap();
+                let card_2: &Card = board.get(card_index_2).unwrap();
 
-        let card_0: &Card = board.get(card_index_0).unwrap();
-        let card_1: &Card = board.get(card_index_1).unwrap();
-        let card_2: &Card = board.get(card_index_2).unwrap();
+                println!("You chose {:?}, {:?}, {:?}", card_0, card_1, card_2);
 
-        println!("You chose {:?}, {:?}, {:?}", card_0, card_1, card_2);
+                if is_set(card_0, card_1, card_2) {
+                    let mut indices = vec![card_index_0, card_index_1, card_index_2];
+                    indices.sort();
+                    println!("Nicely done!");
+                    board.remove(indices[2]);
+                    board.remove(indices[1]);
+                    board.remove(indices[0]);
+                } else {
+                    println!("Keep looking!")
+                }
+            }
+        };
+    }
+}
 
-        if is_set(card_0, card_1, card_2) {
-            let mut indices = vec![card_index_0, card_index_1, card_index_2];
-            indices.sort();
-            println!("Nicely done!");
-            board.remove(indices[2]);
-            board.remove(indices[1]);
-            board.remove(indices[0]);
-        } else {
-            println!("Keep looking!")
+fn find_set(board: &Vec<Card>) -> Vec<(usize, usize, usize)> {
+    let mut result = Vec::new();
+    for index_0  in 0..board.len() {
+        for index_1 in (index_0 + 1)..board.len() {
+            for index_2 in (index_1 + 1)..board.len() {
+                let card_0: &Card = board.get(index_0).unwrap();
+                let card_1: &Card = board.get(index_1).unwrap();
+                let card_2: &Card = board.get(index_2).unwrap();
+
+                if is_set(card_0, card_1, card_2) {
+                    result.push((index_0, index_1, index_2));
+                }
+            }
         }
     }
+    return result;
 }
 
 fn print_board(board: &Vec<Card>) {
@@ -109,12 +154,20 @@ fn print_board(board: &Vec<Card>) {
     }
 }
 
-fn read_input() -> (usize, usize, usize) {
+fn read_input() -> InputType {
     let mut raw_user_input = String::new();
 
     io::stdin()
         .read_line(&mut raw_user_input)
         .expect("Failed to read line");
+
+    if raw_user_input.trim() == "deal" {
+        return InputType::Deal;
+    }
+
+    if raw_user_input.trim() == "help" {
+        return InputType::Help;
+    }
 
     let potential_set_indices: Vec<&str> = raw_user_input.split(" ").collect::<Vec<&str>>();
     if potential_set_indices.len() != 3 {
@@ -125,7 +178,7 @@ fn read_input() -> (usize, usize, usize) {
     let card_index_1 = parse_card_index(&potential_set_indices, 1) as usize;
     let card_index_2 = parse_card_index(&potential_set_indices, 2) as usize;
 
-    return (card_index_0, card_index_1, card_index_2);
+    return InputType::PotentialSet(card_index_0, card_index_1, card_index_2);
 }
 
 fn parse_card_index(potential_set_indices: &Vec<&str>, input_index: usize) -> i32 {
